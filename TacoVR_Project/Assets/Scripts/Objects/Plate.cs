0cedba9c92eb6986054292427de3f16a05684017
Aleizqher <Alejandro.izqher@gmail.com>
2022-08-28 03:31:56 -0500

    Plate Script

---
+++ TacoVR_Project/Assets/Scripts/Objects/Plate.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+    public GameObject TortillaEmpty, MeetEmpty, OnionEmpty, CilantroEmpty, SalsaEmpty;
+    private bool EmptyPlate, TortillaInPlate, MeetInPlate, CilantroInPlate, OnionInPlate,SalsaInPlate ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EmptyPlate = false;
+        TortillaInPlate = false;
+        MeetInPlate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (EmptyPlate! && other.tag == ("Tortilla"))
+        {
+            other.transform.parent = TortillaEmpty.transform;
+            EmptyPlate = true;
+            TortillaInPlate = true;
+            other.transform.position = Vector3.zero;
+
+        }
+        else if (TortillaInPlate && other.tag == ("Meet"))
+        {
+            other.transform.parent = MeetEmpty.transform;
+            MeetInPlate = true;
+            other.transform.position = Vector3.zero;
+
+        }
+        else if (MeetInPlate && other.tag == ("Cilantro"))
+        {
+            other.transform.parent = CilantroEmpty.transform;
+            other.transform.position = Vector3.zero;
+
+        }
+        else if(MeetInPlate && other.tag == ("Onion"))
+        {
+            other.transform.parent = OnionEmpty.transform;
+            other.transform.position = Vector3.zero;
+
+        }
+        else if (MeetInPlate && other.tag == ("Salsa"))
+        {
+            other.transform.parent = SalsaEmpty.transform;
+            other.transform.position = Vector3.zero;
+
+        }
+
+    }
+}
